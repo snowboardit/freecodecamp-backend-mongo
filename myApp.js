7fc44c70bf02fcdbcbd9c3ae731f784d28595b60
Max Lareau <max@maxlareau.com>
2023-06-04 21:42:46 -0400

    Add find person by id

--- myApp.js
+++ myApp.js
@@ -54,7 +54,10 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, (err, data) => {
+    if (err) console.error(err);
+    done(null, data);
+  })
 };
 
 const findEditThenSave = (personId, done) => {
